daf04a47b098ed66df2bd68f3018a81b94559823
Ghashia Mukhtar <ghashiamukhtar9@gmail.com>
2026-07-18 21:13:43 +0500

    Implement median string calculation from file input

---
+++ Median String Problem/main.py
@@ -0,0 +1,8 @@
+from MedianSting import MedianString
+path = input("Enter file path: ")
+with open(path) as f:
+    k = int(f.readline().strip())
+    
+    Dna = f.read().strip().upper().split()
+
+print(MedianString(Dna, k))
