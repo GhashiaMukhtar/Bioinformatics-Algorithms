47c04c82e178b983e8f151698e1b7873e5b90153
Ghashia Mukhtar <ghashiamukhtar9@gmail.com>
2026-07-19 17:35:56 +0500

    Implement Greedy Motif Search in main.py

---
+++ Greedy Motif Search/main.py
@@ -0,0 +1,12 @@
+from GreedyMotif import GreedyMotifSearchWithPseudocounts
+path = input("Enter file path: ")
+with open(path) as f:
+    first_line = f.readline().strip().split()
+    k = int(first_line[0])
+    t = int(first_line[1])
+    Dna = f.read().strip().upper().split()
+
+winners = GreedyMotifSearchWithPseudocounts(Dna, k, t)
+
+for motif in winners:
+    print(" ".join(motif))
