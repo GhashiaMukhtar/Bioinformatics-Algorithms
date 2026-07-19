0e2d4db962506f695dd1caefd6f4a3b18257ef26
Ghashia Mukhtar <ghashiamukhtar9@gmail.com>
2026-07-19 17:39:56 +0500

    Add BuildProfile (no pseudocounts) and keep BuildProfileWithPseudocounts

--- Greedy Motif Search/Profile.py
+++ Greedy Motif Search/Profile.py
@@ -1,3 +1,20 @@
+def BuildProfile(Motifs):
+    k = len(Motifs[0])
+    t = len(Motifs)
+
+    profile = {'A': [0.0]*k, 'C': [0.0]*k, 'G': [0.0]*k, 'T': [0.0]*k}
+    
+    for i in range(k):
+        for motif in Motifs:
+            profile[motif[i]][i] += 1.0
+
+    for nucleotide in "ACGT":
+        for i in range(k):
+            profile[nucleotide][i] = profile[nucleotide][i] / t
+            
+    return profile
+
+
 def BuildProfileWithPseudocounts(Motifs):
     k = len(Motifs[0])
     t = len(Motifs)
